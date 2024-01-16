736faa842f028010416a934a8efc23993bc5c08d
Cougar's the Name <ghostcat@byui.edu>
2024-01-16 11:09:47 -0700

    Base Base Game

--- Demo Game/Opponent/opponent.gd
+++ Demo Game/Opponent/opponent.gd
@@ -1,28 +1,15 @@
 extends CharacterBody2D
 
 
-var Speed = 250
-var ball
-
-
-func _ready():
-	ball = get_parent().get_node("ball")
-	
+const SPEED = 400.0
 func _physics_process(_delta):
-	var direction = _get_opponent_direction()
+	var direction
+	if Input.is_key_pressed(KEY_W):
+		direction = -1
+	elif Input.is_key_pressed(KEY_S):
+		direction = 1
 	if direction:
-		velocity.y = direction * Speed
+		velocity.y = direction * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, Speed)
-	#move_and_collide(velocity)
-	move_and_collide(Vector2(0, _get_opponent_direction()) * Speed)
-
-func _get_opponent_direction():
-	if abs(ball.position.y - position.y) > 25:
-		if ball.position.y > position.y:
-			return 1
-		else:
-			return -1
-	else:
-		return 0
-
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	move_and_slide()
