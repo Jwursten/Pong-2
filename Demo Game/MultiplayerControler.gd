d2838a8caa9be9553f90f01a7928094cb8a2420b
hutonahill <Delaware.white@gmail.com>
2024-02-01 08:19:27 -0700

    tinkering

--- Demo Game/MultiplayerControler.gd
+++ Demo Game/MultiplayerControler.gd
@@ -1,49 +1,80 @@
 extends Node2D
 
-@export var Address = "111.111.111.111"
+var PlayerName = ""
 
 var JoinNodes = []
 
+var filePath = "user://playerName.json"
+
 var errorMsg = ""
 
+
+@export var Address = "111.111.111.111"
 var port = 3944
+
+var Compresstion = ENetConnection.COMPRESS_NONE
 
 var peer
 
 var IpInputNode 
 var IpLabelNode 
-var ConnectButtonNode 
+var ConnectButtonNode
+
+var PlayerNameInputNode
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Establish come connections
 	multiplayer.peer_connected.connect(PlayerConnected)
 	multiplayer.peer_disconnected.connect(PlayerDisconnected)
 	multiplayer.connected_to_server.connect(ConnectedToServer)
 	multiplayer.connection_failed.connect(ConnectionFailed)
 
+	# define join nodes
 	IpInputNode = get_node("%IpInput")
 	IpLabelNode = get_node("%IpLabel")
 	ConnectButtonNode = get_node("%ConnectButton")
-
-	
 
 	JoinNodes = [IpInputNode, IpLabelNode, ConnectButtonNode]
 
 	hideJoinNodes()
 
+	# get the name 
+	var file = FileAccess.open(filePath, FileAccess.READ)
+
+	PlayerName = file.get_as_text()
+
+	file.close()
+
+	PlayerNameInputNode = get_node("%NameInput")
+
+	PlayerNameInputNode.text = PlayerName
+	
+
+
+func saveName(name:String) -> void:
+	
+	PlayerName = name
+
+	var file = FileAccess.open(filePath, FileAccess.WRITE)
+
+	file.store_string(PlayerName)
+
+	file.close()
+
 
 func PlayerConnected(id):
-	print("Player #" + id + " Connected.")
+	PrintToUser("Player #" + str(id) + " Connected.")
 
 func PlayerDisconnected(id):
-	print("Player #" + id + " Disconnected.")
+	PrintToUser("Player #" + str(id) + " Disconnected.")
 
 func ConnectedToServer():
-	print("Connected to Server")
+	PrintToUser("Connected to Server")
 
 func ConnectionFailed():
-	print("Failed Connection")
+	PrintToUser("Failed Connection")
 
 func _on_start_button_pressed():
 	pass # Replace with function body.
@@ -68,19 +99,14 @@ func _on_host_button_pressed():
 	var error = peer.create_server(port, 4)
 
 	if (error != OK):
-		print("Cannot Host: `" + error + "`")
+		PrintToUser("Cannot Host: `" + error + "`")
 		return
 	
-	peer.get_host().compress(ENetConnection.COMPRESS_NONE)
+	peer.get_host().compress(Compresstion)
 
 	multiplayer.set_multiplayer_peer(peer)
-	print("Waiting for Players")
+	PrintToUser("Waiting for Players")
 
-
-
-
-func _on_name_label_focus_exited():
-	pass # Replace with function body.
 
 
 func _on_connect_pressed():
@@ -88,7 +114,11 @@ func _on_connect_pressed():
 	Address = IpInputNode.text
 
 	if(ValidateIp(Address) == true):
-		pass
+		peer = ENetConnection.new()
+		peer.create_cliant(Address, port)
+		peer.get_host().compress(Compresstion)
+		multiplayer.set_multyplay_peer(peer)
+
 
 
 	pass # Replace with function body.
@@ -101,22 +131,22 @@ func ValidateIp(ipAddress:String) -> bool:
 
 	for octet in splitAddress:
 		if (octet.length() == 0 or octet.length() > 3):
-			print("Each octet of the Ip address must be between 1 and 3 digits." +
+			PrintToUser("Each octet of the Ip address must be between 1 and 3 digits." +
 			" (see octet " + counter + ")")
 			return false
 		
 		elif (int(octet) < 0):
-			print("each octet of the Ip Address must be greater than 0" +
+			PrintToUser("each octet of the Ip Address must be greater than 0" +
 			" (see octet " + counter + ")")
 			return false
 		
 		elif (int(octet) > 255):
-			print("each octet of the Ip Address must be less than 254" +
+			PrintToUser("each octet of the Ip Address must be less than 254" +
 			" (see octet " + counter + ")")
 			return false
 		
 		elif(counter > 4):
-			print("There may only be 4 octets in the Ip Address")
+			PrintToUser("There may only be 4 octets in the Ip Address")
 			return false
 	
 	return true
@@ -125,3 +155,13 @@ func ValidateIp(ipAddress:String) -> bool:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func PrintToUser(msg:String) -> void:
+	print(msg)
+
+func _on_name_input_text_changed(new_text:String):
+	saveName(new_text)
+
+
+func _on_name_input_focus_exited():
+	saveName(PlayerNameInputNode.text)
