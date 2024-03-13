83c03d008160f97b3e7f5f7c14bd3405a53bff2f
Jwursten <jwursten@gmail.com>
2024-03-13 11:16:53 -0600

    mode done

--- Demo Game/StartButten.gd
+++ Demo Game/StartButten.gd
@@ -1,5 +1,5 @@
 extends Button
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://ArcadeStage/arcade_stage.tscn")
+	get_tree().change_scene_to_file("res://mode_select/mode_select.tscn")
 
